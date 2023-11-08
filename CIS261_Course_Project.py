9dc3b7e7fef457dd4d47caae20f477d4881c425f
James Alan Bush <139084557+JamesBushStrayer@users.noreply.github.com>
2023-11-08 15:04:59 -0500

    Update CIS261_Course_Project.py
    Updated variable and function names for compliance with Python standard naming conventions

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -1,97 +1,106 @@
 from datetime import datetime
 
+class Employee:
+    def __init__(self, start_date, end_date, name, hours, rate, tax_rate):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.name = name
+        self.hours = hours
+        self.rate = rate
+        self.tax_rate = tax_rate
 
-def get_date_range():
-  from_date = input("Enter the 'From' date (mm/dd/yyyy): ")
-  to_date = input("Enter the 'To' date (mm/dd/yyyy): ")
-  return from_date, to_date
+    @staticmethod
+    def get_date_range():
+        start_date = input("Enter the 'From' date (mm/dd/yyyy): ")
+        end_date = input("Enter the 'To' date (mm/dd/yyyy): ")
+        return start_date, end_date
 
+    @staticmethod
+    def get_employee_name():
+        return input("Enter employee name: ")
 
-def get_employee_name():
-  return input("Enter employee name: ")
+    @staticmethod
+    def get_total_hours():
+        return float(input("Enter total hours: "))
 
+    @staticmethod
+    def get_hourly_rate():
+        return float(input("Enter hourly rate: "))
 
-def get_total_hours():
-  return float(input("Enter total hours: "))
-
-
-def get_hourly_rate():
-  return float(input("Enter hourly rate: "))
-
-
-def get_tax_rate():
-  return float(input("Enter income tax rate: "))
-
-
-def calculate_pay(hours, rate, tax_rate):
-  gross_pay = hours * rate
-  income_tax = gross_pay * (tax_rate / 100)
-  net_pay = gross_pay - income_tax
-  return gross_pay, income_tax, net_pay
-
-
-def create_employee_record(from_date, to_date, name, hours, rate, tax_rate):
-  gross_pay, income_tax, net_pay = calculate_pay(hours, rate, tax_rate)
-  return [
-      from_date, to_date, name, hours, rate, gross_pay, tax_rate, income_tax,
-      net_pay
-  ]
-
-
+    @staticmethod
+    def get_income_tax_rate():
+        return float(input("Enter income tax rate: "))
+  
+    def calculate_pay(self):
+        gross_pay = self.hours * self.rate
+        income_tax = gross_pay * (self.tax_rate / 100)
+        net_pay = gross_pay - income_tax
+        return {"gross_pay": gross_pay, "income_tax" : income_tax, "net_pay": net_pay}
+      
+    def create_employee_data(self):
+        pay = self.calculate_pay()
+        return {
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "employee_name": self.name,
+            "hours" : self.hours,
+            "rate" : self.rate,
+            "gross_pay" : pay["gross_pay"],
+            "tax_rate" : self.tax_rate,
+            "income_tax" : pay["income_tax"],
+            "net_pay": pay["net_pay"]
+            }
+  
 def calculate_totals(records):
-  totals = {
-      "total_employees": len(records),
-      "total_hours": sum(record[3] for record in records),
-      "total_gross_pay": sum(record[5] for record in records),
-      "total_tax": sum(record[7] for record in records),
-      "total_net_pay": sum(record[8] for record in records)
-  }
-  return totals
+    totals = {
+        "employees": len(records),
+        "hours": sum(record['hours'] for record in records),
+        "gross_pay": sum(record['gross_pay'] for record in records),
+        "tax": sum(record['income_tax'] for record in records),
+        "net_pay": sum(record['net_pay'] for record in records)
+    }
+    return totals
 
-
-def display_employee_info(record):
-  print("\nEmployee Information")
-  print("From Date: ", record[0])
-  print("To Date: ", record[1])
-  print("Employee Name: ", record[2])
-  print("Total Hours: ", record[3])
-  print("Hourly Rate: ", record[4])
-  print("Gross Pay: ", record[5])
-  print("Income Tax Rate: ", record[6])
-  print("Income Tax: ", record[7])
-  print("Net Pay: ", record[8])
-
+def display_employee_data(employee_data):
+    print("\nEmployee Information")
+    print(f"From Date: {employee_data['start_date']}")
+    print(f"To Date: {employee_data['end_date']}")
+    print(f"Employee Name: {employee_data['employee_name']}")
+    print(f"Total Hours: {employee_data['hours']}")
+    print(f"Hourly Rate: {employee_data['rate']}")
+    print(f"Gross Pay: {employee_data['gross_pay']}")
+    print(f"Income Tax Rate: {employee_data['tax_rate']}")
+    print(f"Income Tax: {employee_data['income_tax']}")
+    print(f"Net Pay: {employee_data['net_pay']}")
 
 def display_summary(totals):
-  print("\nSummary")
-  print("Total Employees: ", totals["total_employees"])
-  print("Total Hours: ", totals["total_hours"])
-  print("Total Gross Pay: ", totals["total_gross_pay"])
-  print("Total Tax: ", totals["total_tax"])
-  print("Total Net Pay: ", totals["total_net_pay"])
-
+    print("\nSummary")
+    print(f"Total Employees: {totals['employees']}")
+    print(f"Total Hours: {totals['hours']}")
+    print(f"Total Gross Pay: {totals['gross_pay']}")
+    print(f"Total Tax: {totals['tax']}")
+    print(f"Total Net Pay: {totals['net_pay']}")
 
 def main():
-  records = []
+    records = []
 
-  while True:
-    from_date, to_date = get_date_range()
-    name = get_employee_name()
-    if name.lower() == "end":
-      break
+    while True:
+        start_date, end_date = Employee.get_date_range()
+        name = Employee.get_employee_name()
+        if name.lower() == "end":
+            break
 
-    hours = get_total_hours()
-    rate = get_hourly_rate()
-    tax_rate = get_tax_rate()
+        hours = Employee.get_total_hours()
+        rate = Employee.get_hourly_rate()
+        tax_rate = Employee.get_income_tax_rate()
 
-    record = create_employee_record(from_date, to_date, name, hours, rate,
-                                    tax_rate)
-    records.append(record)
-    display_employee_info(record)
+        employee = Employee(start_date, end_date, name, hours, rate, tax_rate)
+        employee_data = employee.create_employee_data()
+        records.append(employee_data)
+        display_employee_data(employee_data)
 
-  totals = calculate_totals(records)
-  display_summary(totals)
-
+    totals = calculate_totals(records)
+    display_summary(totals)
 
 if __name__ == "__main__":
-  main()
+    main()
